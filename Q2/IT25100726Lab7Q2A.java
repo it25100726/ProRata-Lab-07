66876658bdafc76efda0be8022b96b94dcc90f57
it25100726 <it25100726@my.sliit.lk>
2026-09-11 16:22:06 +0530

    Add files via upload
    IT25100726 Lab7 Q2A , Q2B , Q2C

---
+++ Q2/IT25100726Lab7Q2A.java
@@ -0,0 +1,7 @@
+public class IT25100726Lab7Q2A {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("$$$$$");
+        }
+    }
+}
---
+++ Q2/IT25100726Lab7Q2C.java
@@ -0,0 +1,10 @@
+public class IT25100726Lab7Q2C {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
